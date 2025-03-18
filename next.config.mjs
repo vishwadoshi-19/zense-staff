1fa1a9fe0c65e7aff2449a85ebf2c9091dc99b8c
VISHWA SMITEN DOSHI <99802964+vishwadoshi-19@users.noreply.github.com>
2025-03-18 15:17:30 +0530

    pwa functionality

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,21 @@
 /** @type {import('next').NextConfig} */
+import withPWA from "next-pwa";
+
 const nextConfig = {
+  distDir: "build",
   reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    // removeConsole: process.env.NODE_ENV !== "development",
+  },
   images: {
     domains: ["firebasestorage.googleapis.com", "images.unsplash.com"],
   },
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  // disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+})(nextConfig);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,74 @@
-"use client";
-
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Navigation } from "@/components/Navigation";
 import { AuthProvider } from "@/context/AuthContext";
 import { Toaster } from "react-hot-toast";
 import { usePathname } from "next/navigation";
+import { Metadata, Viewport } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
 
-// export const metadata: Metadata = {
-//   title: "Zense Staff Portal",
-//   description: "Portal for staff to manage assignments and tasks",
-// };
+<meta name="apple-mobile-web-app-title" content="Zense Staff" />;
+
+export const metadata: Metadata = {
+  title: "Zense Staff Portal",
+  description: "Portal for staff to manage assignments and tasks",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["staff, portal, assignments, tasks", "nextjs", "next14"],
+  authors: [
+    {
+      name: "Vishwa Doshi",
+      url: "https://github.com/vishwadoshi-19",
+    },
+  ],
+  icons: [
+    {
+      rel: "apple-touch-icon",
+      type: "image/png",
+      url: "icons/web-app-manifest-512x512-512.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      url: "icons/web-app-manifest-512x512-512.png",
+    },
+  ],
+};
+
+export const viewport: Viewport = {
+  themeColor: "white",
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
-  const showNavigation = !["/sign-in", "/onboarding"].includes(pathname);
+  // const pathname = usePathname();
+  // const showNavigation = !["/sign-in", "/onboarding"].includes(pathname);
 
   return (
     <html lang="en">
+      <head>
+        <meta name="theme-color" content="#ffffff" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="icons/web-app-manifest-512x512-512.png" />
+        <link
+          rel="apple-touch-icon"
+          href="icons/web-app-manifest-512x512-512.png"
+        />
+      </head>
       <body className={inter.className}>
         <AuthProvider>
           <div className="min-h-screen bg-gray-50">
             {children}
-            {showNavigation && <Navigation />}
+            {/* {showNavigation && <Navigation />} */}
+            <Navigation />
             <Toaster position="top-center" />
           </div>
         </AuthProvider>
